1bca2d830aa2455c15a83280097635c43fbb831c
Mochammad Farros Fatchur Roji <farros.alfatih@gmail.com>
2025-08-15 07:30:55 +0700

    jos

--- src/components/complexFooter.tsx
+++ src/components/complexFooter.tsx
@@ -4,10 +4,10 @@ import StoreDoubleColumn from '../components/store/storeDoubleColumn';
 export default function ComplexFooter() {
   return (
     <>
-      <footer>
+      <footer className="container">
         <div className="row">
           <div className="col-12 col-md-4">
-            <h5 className="mt-5">Astro Ecommerce</h5>
+            <h5 className="mt-5">SonusHUB</h5>
             <p className="text-body text-sm pe-5">The time is now for it to be okay to be great. People in this world shun people for being great. For being a bright color. For standing out.</p>
           </div>
           <div className="col-12 col-md-8">
@@ -15,12 +15,7 @@ export default function ComplexFooter() {
           </div>
           <div className="col-12 mt-5 mb-lg-0 mb-4">
             <div className="copyright text-center text-sm text-body">
-              Copyright © 
-              <script>
-                document.write(new Date().getFullYear())
-              </script>
-              &nbsp;Astro Ecommerce by &nbsp;
-              <a href="https://www.creative-tim.com" className="text-dark" target="_blank">Creative Tim</a>.
+              Developed by <a href="https://farrosfr.com" className="text-dark" target="_blank">FarrosFR (https://farrosfr.com)</a>
             </div>
           </div>
         </div>
